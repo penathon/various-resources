17da3421cda828fec17918b837b55622e04d3c2b
penathon <61853640+penathon@users.noreply.github.com>
2024-05-21 15:51:43 +0100

    clean-up and optimization

--- impounds/cl_main.lua
+++ impounds/cl_main.lua
@@ -1,88 +1,109 @@
 local impound = {}
+local impoundLogs = {}
 local menu_options = {}
+local menu_options2 = {}
 local impoundLocation = nil
 
-function spawnCar(id, car, properties, reason)
-    if lib.progressBar({
-        duration = 5000,
-        label = 'Spawning Vehicle',
-        useWhileDead = false,
-        canCancel = true,
-        disable = {
-            car = true,
-            move = true
-        },
-    }) then 
-        local p = PlayerPedId()
-
-        RequestModel(car)
-        while not HasModelLoaded(car) do
-            RequestModel(car)
-            Citizen.Wait(0)
-        end
-
-        for location, data in pairs(Config.Locations) do
-            for i, spot in ipairs(data.zones) do
-                if spot.name == impoundLocation then
-                    local vehicle = CreateVehicle(car, spot.carSpawn.coords, spot.carSpawn.heading, true, false)
-                    lib.setVehicleProperties(vehicle, properties)
-                end
-            end
-        end
-
-
-        TriggerServerEvent('pen-garages:deleteImpound', id, car, properties, reason)
-        menu_options = {}
-        impound = {}
-    else 
-    end
-end
-
 RegisterNetEvent('pen-garages:refreshImpoundData')
-AddEventHandler('pen-garages:refreshImpoundData', function(data)
-    impound = {}
+AddEventHandler('pen-garages:refreshImpoundData', function(data, data2)
     impound = data
-    --print('data refreshed')
-    --print(json.encode(impound))
+    impoundLogs = data2
 end)
 
 Citizen.CreateThread(function()
-    TriggerServerEvent('pen-garages:requestRefresh', GetPlayerServerId(PlayerId()))
+    Citizen.Wait(500)
+    refreshData()
 end)
 
 function registerMenu()
+    refreshData()
+    Citizen.Wait(500)
     for k,v in ipairs(impound) do
         table.insert(menu_options, {
             title = 'ID - ' .. v.id .. ' Date - ' .. v.date .. '',
             description = 'Plate - ' .. v.vehProperties.plate .. ' Model - ' .. v.vehModel .. '',
             onSelect = function(args)
-                spawnCar(v.id, v.vehModel, v.vehProperties, v.reason)
+                spawnCar(v.id, v.vehModel, v.vehProperties, v.reason, v.offender, v.impoundee)
                 menu_options = {}     
             end,
             metadata = {
                 {label = 'Offender', value = v.offender},
+                {label = 'Impoundee', value = v.impoundee},
                 {label = 'Reason', value = v.reason}
             }
         })
     end
 
+    for k,v in ipairs(impoundLogs) do
+        table.insert(menu_options2, {
+            title = 'ID - ' .. v.id .. ' Date - ' .. v.date .. '',
+            description = 'Reason - ' .. v.reason .. '',
+            metadata = {
+                {label = 'Offender', value = v.offender},
+                {label = 'Impoundee', value = v.impoundee}
+            }
+        })
+    end
+
     lib.registerContext({
-        id = 'impound_menu',
-        title = 'Impound',
+        id = 'impound_menu2',
+        title = 'Impound Logs',
+        menu = 'impound_menu',
         onExit = function()
             menu_options = {}
+            menu_options2 = {}
+        end,
+        onBack = function()
+            menu_options = {}
+            menu_options2 = {}
+        end,
+        options = menu_options2
+    })
+
+    lib.registerContext({
+        id = 'impound_menu3',
+        title = 'Current Impounds',
+        menu = 'impound_menu',
+        onExit = function()
+            menu_options = {}
+            menu_options2 = {}
+        end,
+        onBack = function()
+            menu_options = {}
+            menu_options2 = {}
         end,
         options = menu_options
     })
+
+    lib.registerContext({
+        id = 'impound_menu',
+        title = 'Impound Menu',
+        onExit = function()
+            menu_options = {}
+            menu_options2 = {}
+        end,
+        onBack = function()
+            menu_options = {}
+            menu_options2 = {}
+        end,
+        options = {
+            {
+                title = 'Current Impounds',
+                description = 'View Current Impounds',
+                menu = 'impound_menu3',
+                arrow = true
+            },
+            {
+                title = 'Impounds Log',
+                description = 'View Previous Impounds',
+                menu = 'impound_menu2',
+                arrow = true
+            }
+        }
+    })
+
     lib.showContext('impound_menu')
 end
-
-RegisterCommand('impound', function()
-    registerMenu()
-    lib.showContext('impound_menu')
-end)
-
-
 
 for location, data in pairs(Config.Locations) do
     for i, spot in ipairs(data.zones) do
@@ -119,4 +140,45 @@ function checkIfDuty()
     local state = LocalPlayer.state.clockin
     local clockedin = state.isLeo
     return clockedin
+end
+
+function deleteImpound(id, car, properties, reason, offender, impoundee)
+    TriggerServerEvent('pen-garages:deleteImpound', id, car, properties, reason, offender, impoundee)
+end
+
+function spawnCar(id, car, properties, reason, offender, impoundee)
+    if lib.progressBar({
+        duration = 5000,
+        label = 'Spawning Vehicle',
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            car = true,
+            move = true
+        },
+    }) then 
+        local p = PlayerPedId()
+
+        RequestModel(car)
+        while not HasModelLoaded(car) do
+            RequestModel(car)
+            Citizen.Wait(0)
+        end
+
+        for location, data in pairs(Config.Locations) do
+            for i, spot in ipairs(data.zones) do
+                if spot.name == impoundLocation then
+                    local vehicle = CreateVehicle(car, spot.carSpawn.coords, spot.carSpawn.heading, true, false)
+                    lib.setVehicleProperties(vehicle, properties)
+                end
+            end
+        end
+
+
+        deleteImpound(id, car, properties, reason, offender, impoundee)
+    end
+end
+
+function refreshData()
+    TriggerServerEvent('pen-garages:requestRefresh', GetPlayerServerId(PlayerId()))
 end
--- impounds/sv_main.lua
+++ impounds/sv_main.lua
@@ -1,30 +1,15 @@
 impound = {}
-
+impoundLogs = {}
 
 Citizen.CreateThread(function()
     dbSync()
 end)
 
-RegisterNetEvent('pen-garages:saveImpound')
-AddEventHandler('pen-garages:saveImpound', function(properties, netId, reason, offender)
-    local random = math.random(1,10000)
-    saveToDB(random, properties.model, json.encode(properties), reason, offender)
-end)
-
-function saveToDB(id, model, properties, reason, offender)
-    local date = os.date('%Y-%m-%d %H:%M:%S')
-    local trunk = "NONE"
-    MySQL.Async.execute("INSERT INTO pen_garages (id, model, properties, trunk, date, reason, offender) VALUES (@id, @model, @properties, @trunk, @date, @reason, @offender)", {["@id"] = id, ["@model"] = model, ["@properties"] = properties, ["@trunk"] = trunk, ["@date"] = date, ["@reason"] = reason, ["@offender"] = offender})
-    dbSync()
-end
-
 function dbSync()
-    impound = {}
     MySQL.query('SELECT * FROM `pen_garages`', function(response)
         if response then
             for i = 1, #response do
                 local row = response[i]
-                --print(row.firstname, row.lastname)
                 table.insert(impound, {
                     id = tonumber(row.id),
                     vehModel = tonumber(row.model),
@@ -32,69 +17,86 @@ function dbSync()
                     trunk = row.trunk,
                     date = row.date,
                     reason = row.reason,
-                    offender = row.offender
+                    offender = row.offender,
+                    impoundee = row.impoundee
                 })
-                --print(json.encode(impound))
+                print('found row')
             end
         end
-        sendRefresh(-1)
-    end)
-end
-
-function deleteCar(id, car, properties, reason)
-    MySQL.prepare('DELETE FROM `pen_garages` WHERE `id` = ?', {
-        id
-    }, function(response)
-        --print(json.encode(response, { indent = true, sort_keys = true }))
-        --print('deleted')
     end)
 
-    local date = os.date('%Y-%m-%d %H:%M:%S')
-    MySQL.Async.execute("INSERT INTO pen_garages_logs (id, model, properties, reason, date) VALUES (@id, @model, @properties, @reason, @date)", {["@id"] = id, ["@model"] = model, ["@properties"] = properties, ["@reason"] = reason, ["@date"] = date})
-    
-    removeFromDatabase(id)
-    dbSync()
+    MySQL.query('SELECT * FROM `pen_garages_logs`', function(response)
+        if response then
+            for i = 1, #response do
+                local row = response[i]
+                table.insert(impoundLogs, {
+                    id = tonumber(row.id),
+                    vehModel = tonumber(row.model),
+                    date = row.date,
+                    reason = row.reason,
+                    offender = row.offender,
+                    impoundee = row.impoundee
+                })
+            end
+        end
+    end)
+    print('synced')
     sendRefresh(-1)
 end
 
-function findKey(id)
-    for key, value in ipairs(impound) do
-        if tonumber(value.id) == tonumber(id) then
-          return key
-        end
-    end
+function deleteCar(id, car, properties, reason, offender, impoundee)
+    removeFromDatabase(id)
+    addToLog(id, car, properties, reason, offender, impoundee)
+    Citizen.Wait(1000)
+    dbSync()
+end
+
+function saveToDB(id, model, properties, reason, offender, impoundee)
+    local date = os.date('%Y-%m-%d %H:%M:%S')
+    local trunk = "NONE"
+    MySQL.Async.execute("INSERT INTO pen_garages (id, model, properties, trunk, date, reason, offender, impoundee) VALUES (@id, @model, @properties, @trunk, @date, @reason, @offender, @impoundee)", {["@id"] = id, ["@model"] = model, ["@properties"] = properties, ["@trunk"] = trunk, ["@date"] = date, ["@reason"] = reason, ["@offender"] = offender, ["@impoundee"] = impoundee})
+    Citizen.Wait(1000)
+    dbSync()
 end
 
 function removeFromDatabase(id)
-    local key = findKey(id)
+    local key = findKey(id, impound)
+    MySQL.prepare('DELETE FROM `pen_garages` WHERE `id` = ?', { id }, function(response) end)
     table.remove(impound, key)
 end
 
-
-
---[[RegisterCommand('testspawn', function(source, id)
-    for k,v in ipairs(impound) do
+function findKey(id, table)
+    for k,v in ipairs(table) do
         if v.id == id then
-            TriggerClientEvent('pen-garages:spawnImpound', source, v.vehModel, v.vehProperties)
+            return k
         end
     end
-end)]]--
-
--- Send to client
-
-
+end
 
 function sendRefresh(playerId)
+    TriggerClientEvent('pen-garages:refreshImpoundData', playerId, impound, impoundLogs)
+end
 
-    TriggerClientEvent('pen-garages:refreshImpoundData', playerId, impound)
+function addToLog(id, model, properties, reason, offender, impoundee)
+    print(id, model, properties, reason, offender, impoundee)
+    local date = os.date('%Y-%m-%d %H:%M:%S')
+    MySQL.Async.execute("INSERT INTO pen_garages_logs (id, model, reason, date, offender, impoundee) VALUES (@id, @model, @reason, @date, @offender, @impoundee)", {["@id"] = id, ["@model"] = model, ["@reason"] = reason, ["@date"] = date, ["@offender"] = offender, ["@impoundee"] = impoundee})
 end
 
 RegisterNetEvent('pen-garages:requestRefresh')
 AddEventHandler('pen-garages:requestRefresh', function(playerServerId)
     sendRefresh(playerServerId)
+    print(json.encode(impound, { indent = true, sort_keys = true }))
 end)
 
 RegisterNetEvent('pen-garages:deleteImpound')
-AddEventHandler('pen-garages:deleteImpound', function(id, car, properties, reason)
-    deleteCar(id, car, properties, reason)
+AddEventHandler('pen-garages:deleteImpound', function(id, car, properties, reason, offender, impoundee)
+    deleteCar(id, car, properties, reason, offender, impoundee)
+end)
+
+RegisterNetEvent('pen-garages:saveImpound')
+AddEventHandler('pen-garages:saveImpound', function(properties, netId, reason, offender)
+    local src = source
+    local random = math.random(1,10000)
+    saveToDB(random, properties.model, json.encode(properties), reason, offender, GetPlayerName(src))
 end)
